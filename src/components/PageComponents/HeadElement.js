d992376ad9b14f093608b07c764264fba12e3d07
Kaiwen Wang <kvnwkw@gmail.com>
2023-05-04 00:22:24 -0400

    opengraph stuff

--- src/components/PageComponents/HeadElement.js
+++ src/components/PageComponents/HeadElement.js
@@ -1,20 +1,26 @@
-
-
-
-
 import Head from "next/head";
 
 export default function HeadElement({ title, desc }) {
-    return (
-        <Head>
-            <title>{title}</title>
-            <meta name="description" content={desc} />
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            {/* <link rel="icon" href="/favicon.ico" /> */}
-            <link
-                rel="icon"
-                href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🐝</text></svg>"
-            />
-        </Head>
-    )
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta name="description" content={desc} />
+      <meta
+        property="og:title"
+        content="Georgia Tech Subleaser | Midtown, Home Park, Atlantic Station, and more"
+      />
+      <meta
+        property="og:description"
+        content="Subleases in Midtown Atlanta by GT students who are graduating, studying abroad, or interning. No spam, modern tech, easy to use."
+      />
+      <meta property="og:image" content="/public/opengraph.jpeg" />
+
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      {/* <link rel="icon" href="/favicon.ico" /> */}
+      <link
+        rel="icon"
+        href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🐝</text></svg>"
+      />
+    </Head>
+  );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,24 @@
-import Header from "@/components/PageComponents/Header";
-import EmblaCarousel from "@/components/Carousel/Carousel";
-
-import { Auth } from "@supabase/auth-ui-react";
-import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { AppContext } from "/src/components/AppState.js";
 // import { useSession, useSupabaseClient } from "@supabase/auth-helpers-react";
 import Account from "@/components/Account.js";
-
+import EmblaCarousel from "@/components/Carousel/Carousel";
 import FilteredGrid from "@/components/PageComponents/FilteredGrid";
 import HeadElement from "@/components/PageComponents/HeadElement";
-
-import { AppContext } from "/src/components/AppState.js";
+import Header from "@/components/PageComponents/Header";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
 import { useContext } from "react";
+import useSWR from "swr";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
-import useSWR from 'swr'
-
-
-const fetcher = async url => {
+const fetcher = async (url) => {
   const res = await fetch(url);
 
   if (!res.ok) {
-    const error = new Error('An error occurred while fetching the data.');
+    const error = new Error("An error occurred while fetching the data.");
     error.status = res.status;
     error.info = await res.json();
     throw error;
@@ -33,17 +28,29 @@ const fetcher = async url => {
 };
 
 export default function Home() {
-  let { semesterPreference, maxPrice, genderPreference, bathroomPreference, maxRoommates, moveIn, moveOut } = useContext(AppContext);
+  let {
+    semesterPreference,
+    maxPrice,
+    genderPreference,
+    bathroomPreference,
+    maxRoommates,
+    moveIn,
+    moveOut,
+  } = useContext(AppContext);
   let { sortFormula } = useContext(AppContext);
 
-
   // Apparently data changes when the context changes. Is this normal?
-  const { data, error } = useSWR(`/api/FilteredPostsApi?semester=${semesterPreference}&price=${maxPrice}&gender=${genderPreference}&bathroom=${bathroomPreference}&roommates=${maxRoommates}&movein=${moveIn}&moveout=${moveOut}&sort=${sortFormula}`, fetcher)
-
+  const { data, error } = useSWR(
+    `/api/FilteredPostsApi?semester=${semesterPreference}&price=${maxPrice}&gender=${genderPreference}&bathroom=${bathroomPreference}&roommates=${maxRoommates}&movein=${moveIn}&moveout=${moveOut}&sort=${sortFormula}`,
+    fetcher
+  );
 
   return (
     <>
-      <HeadElement title="Georgia Tech Subleaser" desc="The best marketplace for subleases" />
+      <HeadElement
+        title="Georgia Tech Subleaser | Midtown, Home Park, Atlantic Station, and more"
+        desc="Subleases in Midtown Atlanta by GT students who are graduating, studying abroad, or interning. No spam, modern tech, easy to use."
+      />
       <Header showFilters={true} />
       <FilteredGrid postsData={data} error={JSON.stringify(error)} />
     </>
