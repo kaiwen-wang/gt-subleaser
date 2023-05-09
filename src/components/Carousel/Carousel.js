c3ded7ddae1ae5bb15756db79ecde525fe8109a6
Kaiwen Wang <kvnwkw@gmail.com>
2023-05-09 12:13:33 -0400

    uages

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -72,10 +72,10 @@ export default function EmblaCarousel({ supabaseURL, url, freudID }) {
                   fill={true}
                   // width={500}
                   // height={500}
-                  // sizes="(max-width: 768px) 100vw,
-                  // (max-width: 1200px) 50vw,
-                  // 33vw"
-                  //     style={{ objectFit: "cover" }}
+                  sizes="(max-width: 768px) 100vw,
+                  (max-width: 1200px) 50vw,
+                  33vw"
+                  style={{ objectFit: "cover" }}
                 />
               </div>
             ))}
