78a5f7f653ae1e07434c7be37e5718137812a2dd
Kaiwen Wang <kvnwkw@gmail.com>
2023-05-14 23:41:05 -0400

    ios round mobile

--- src/components/PageComponents/SkellyImage.js
+++ src/components/PageComponents/SkellyImage.js
@@ -103,7 +103,7 @@ export default function SkellyImage({ freudID, url, item }) {
         ) : null}
       </div>
       {data ? (
-        <div className=" absolute top-0 z-0 flex items-center justify-center w-full h-full">
+        <div className="absolute top-0 z-0 flex items-center justify-center w-full h-full rounded-full">
           <EmblaCarousel
             supabaseURL={data}
             url={url}
