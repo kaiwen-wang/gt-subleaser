100d2ea3c48f2325d0f0a5b3a6995f820f6ef3dd
Kaiwen Wang <kvnwkw@gmail.com>
2023-05-16 15:13:53 -0400

    oxford comma

--- src/utils/createAnds.js
+++ src/utils/createAnds.js
@@ -3,7 +3,7 @@ export function createAnds(array) {
   if (array.length > 1) {
     let lastItem = array.pop();
     let result =
-      array.join(`${array.length !== 2 ? "," : ""} `) + ", and " + lastItem;
+      array.join(`${array.length === 2 ? "" : ","} `) + " and " + lastItem;
     return result;
   } else {
     return array;
