d68f70a85df27a15972c370e4932115e6639d669
Kaiwen Wang <kvnwkw@gmail.com>
2023-05-28 23:02:12 -0400

    account formatting

--- src/components/Account.js
+++ src/components/Account.js
@@ -5,8 +5,9 @@ export default function Account({ session }) {
   const supabase = useSupabaseClient();
   const user = useUser();
   const [loading, setLoading] = useState(true);
-  const [username, setUsername] = useState(null);
-  const [website, setWebsite] = useState(null);
+
+  const [firstName, setFirstName] = useState(null);
+  const [selfIntroduction, setSelfIntroduction] = useState(null);
   const [avatar_url, setAvatarUrl] = useState(null);
 
   useEffect(() => {
@@ -19,7 +20,7 @@ export default function Account({ session }) {
 
       let { data, error, status } = await supabase
         .from("profiles")
-        .select(`username, website, avatar_url`)
+        .select(`first_name, self_introduction, avatar_url`)
         .eq("id", user.id)
         .single();
 
@@ -28,8 +29,8 @@ export default function Account({ session }) {
       }
 
       if (data) {
-        setUsername(data.username);
-        setWebsite(data.website);
+        setFirstName(data.first_name);
+        setSelfIntroduction(data.self_introduction);
         setAvatarUrl(data.avatar_url);
       }
     } catch (error) {
@@ -40,14 +41,14 @@ export default function Account({ session }) {
     }
   }
 
-  async function updateProfile({ username, website, avatar_url }) {
+  async function updateProfile({ firstName, selfIntroduction, avatar_url }) {
     try {
       setLoading(true);
 
       const updates = {
         id: user.id,
-        username,
-        website,
+        first_name: firstName,
+        self_introduction: selfIntroduction,
         avatar_url,
         updated_at: new Date().toISOString(),
       };
@@ -65,7 +66,17 @@ export default function Account({ session }) {
 
   return (
     <div className="mt-2">
-      <div className=" flex justify-between">
+      <Avatar
+        uid={user.id}
+        url={avatar_url}
+        size={100}
+        onUpload={(url) => {
+          setAvatarUrl(url);
+          updateProfile({ firstName, selfIntroduction, avatar_url: url });
+        }}
+      />
+
+      <div className=" flex justify-between mt-1">
         <label htmlFor="email" className=" font-medium text-gray-800">
           {"Email"}
         </label>
@@ -78,49 +89,50 @@ export default function Account({ session }) {
         />
       </div>
       <div className="flex justify-between">
-        <label htmlFor="username" className="font-medium">
+        <label htmlFor="first_name" className="font-medium">
           First Name
         </label>
         <input
-          className="border border-black"
-          id="username"
+          className="px-1 border border-black"
+          id="first_name"
+          pattern="[A-Za-z]+"
           type="text"
-          value={username || ""}
-          onChange={(e) => setUsername(e.target.value)}
+          title="Enter your first name without any numbers or special characters"
+          value={firstName || ""}
+          onChange={(e) => {
+            // Checks if the input contains any spaces or numbers.
+            if (!/[^a-zA-Z]/.test(e.target.value)) {
+              setFirstName(e.target.value);
+            }
+          }}
         />
       </div>
       <div className="flex flex-col">
-        <label htmlFor="username" className="font-medium">
-          Description
+        <label htmlFor="self_introduction" className="font-medium">
+          Self Introduction
         </label>
         <textarea
           rows="3"
-          className="block w-full border border-black"
-          id="username"
+          maxLength="100"
+          className="px-1 py-0.5 border border-black"
+          id="self_introduction"
           type="text"
-          value={website || ""}
-          onChange={(e) => setWebsite(e.target.value)}
+          value={selfIntroduction || ""}
+          onChange={(e) => setSelfIntroduction(e.target.value)}
         />
       </div>
 
       <div>
         <button
           className="hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 inline-flex justify-center w-full px-4 py-2 mt-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md"
-          onClick={() => updateProfile({ username, website, avatar_url })}
+          onClick={() =>
+            updateProfile({ firstName, selfIntroduction, avatar_url })
+          }
           disabled={loading}
         >
           {loading ? "Loading..." : "Update"}
         </button>
       </div>
-      {/* <Avatar
-        uid={user.id}
-        url={avatar_url}
-        size={100}
-        onUpload={(url) => {
-          setAvatarUrl(url);
-          updateProfile({ username, website, avatar_url: url });
-        }}
-      /> */}
 
       {/* <div>
         <button
